4a24dfac6cf336c3a0c3419b3aed3ae35e6b29c1
Danish37 <57654003+Danish37@users.noreply.github.com>
2020-02-18 13:50:19 +0800

    Add files via upload

--- HTTP.py
+++ HTTP.py
@@ -2,15 +2,15 @@ import requests
 url = "http://172.17.50.43/algenius"
 r = requests.get(url)
 #print(r.text)
-print("Status code:")
-print("\t *",'OK')
+print("Status code: OK")
+print("\t *",r.status_code)
 h = requests.head(url)
 print("Header: \n ********")
 for x in h.headers:
     print("\t",x,":",h.headers[x])
 print("********")
 
-url2 = "http://httpbin.org/headers"
+url2 = "http://172.17.50.43/headers.php"
 headers = {
     'User-Agent':'Mobile'
 }
